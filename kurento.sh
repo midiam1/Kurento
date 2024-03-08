e1b456c25a1b3e8694e210298eddf942ba0b1459
midiam1 <admhimmeros@gmail.com>
2024-03-08 16:42:26 -0400

    Update kurento.sh
    Acá agregamos la instalación de Docker + Portainer + Kuranto

--- kurento.sh
+++ kurento.sh
@@ -41,3 +41,47 @@ timedatectl set-timezone America/Caracas
 
     sudo apt -y install --install-recommends webmin
     echo ' Una vez instalado webmin se debe abrir el puerto 10000 para que acepte las conexiones . '
+
+## Instalación de Docker
+
+## Instalo certificados
+
+    sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo apt-key fingerprint 0EBFCD88
+
+## Añado el repositorio de Docker ::
+
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+## Con esto instalamos la última versión de Docker ::
+
+    apt -y install docker-ce docker-ce-cli containerd.io
+
+## Verificamos que Docker esté instalado ::
+
+    docker run hello-world
+
+## Instalación de docker-compose
+
+    sudo apt -y update
+    sudo apt -y install docker-compose
+    docker-compose --version
+
+## Instalación Portainer
+
+## Por seguridad y control todo será instalado en /home
+
+    mkdir /home/portainer
+    cd /home/portainer
+
+    sudo docker volume create portainer_data
+    sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+    echo ' Posteriormente se debe abrir el puerto 9443 para poder acceder a Portainer . '
+    echo ' Una vez alcanzado el acceso se debe registrar al usuario administrativo . '
+
+## Instalación de docker Kuranto 
+
+    sudo docker pull kurento/kurento-media-server:7.0.0
+    sudo docker run -d --name kurento --network host kurento/kurento-media-server:7.0.0
