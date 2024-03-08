0ad70b302fc7fcecd0c228443a7b404985416c5a
midiam1 <admhimmeros@gmail.com>
2024-03-08 16:30:01 -0400

    Update kurento.sh

--- kurento.sh
+++ kurento.sh
@@ -20,3 +20,24 @@ timedatectl set-timezone America/Caracas
 
   sudo apt -y update
   sudo apt -y upgrade
+    ## Instalo algunas herramientas
+  sudo apt -y install net-tools
+
+# -------------------------------------------------------------------- #
+
+## Importante ::
+
+    clear
+    echo ' Instalo certificados para que la instalación de Webmin no arroje error '
+    sudo apt install ca-certificates -y
+
+## Configuro los repositorios de webmin
+
+    sudo apt -y install curl
+    sudo curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
+    sudo sh setup-repos.sh
+
+## Instalo Webmin
+
+    sudo apt -y install --install-recommends webmin
+    echo ' Una vez instalado webmin se debe abrir el puerto 10000 para que acepte las conexiones . '
